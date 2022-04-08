6d46deffdbcf6152d153aac1a148b0ca249262a0
sunainadevii <92317637+sunainadevii@users.noreply.github.com>
2022-04-08 11:20:04 +0530

    Add files via upload

---
+++ tf-import_impl/gcp_network.tf
@@ -0,0 +1,15 @@
+/*
+Steps to create setup for terraform import implementation:
+1. Create a VPC using below resource block.
+2. Create a subnet manually inside it and then import the subnet.
+*/
+
+# Resource block to deploy VPC Network
+resource "google_compute_network" "tst_vpc" {
+  project = var.project_id
+
+  name                            = var.vpc_name
+  routing_mode                    = "GLOBAL"
+  auto_create_subnetworks         = var.auto_create
+  delete_default_routes_on_create = var.delete_default_routes
+}
